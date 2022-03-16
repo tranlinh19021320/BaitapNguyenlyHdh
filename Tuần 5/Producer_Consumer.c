e039b5c061c9ed371a8d0eb96bf78d3d9de23d3f
TranLinh21 <linhya21@gmail.com>
2022-03-16 16:42:40 +0700

    Tuần 5

--- Tuần 5/Producer_Consumer.c
+++ Tuần 5/Producer_Consumer.c
@@ -1,78 +1,128 @@
-#include <pthread.h>
-#include <semaphore.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
 
-#define BufferSize 10 // Size of the buffer
 
-int empty=BufferSize;
-int full=0;
-int in = 0;
-int out = 0;
-int mutex=1;
-int buffer[BufferSize];
-//pthread_mutex_t mutex;
+typedef int buffer_item;
+#define BUFFER_SIZE 5
 
-void wait(int *a)
-{
-	while(a<=0);
-	a--;
+// Global variables
+buffer_item buffer[BUFFER_SIZE];
+pthread_mutex_t mutex;
+sem_t full, empty;
+int count, in, out;
+
+// Function prototypes
+int insert_item(buffer_item item);
+int remove_item(buffer_item *item);
+void *consumer(void *param);
+void *producer(void *param);
+
+int main(int argc, char **argv){
+  if (argc != 4){
+    printf("ERROR: Provide exactly three arguments.\n");
+    exit(1);
+  }
+
+  // Retrieve command line arguments
+  const long int stime = strtol(argv[1], NULL, 0);
+  const long int num_producer = strtol(argv[2], NULL, 0);
+  const long int num_consumer = strtol(argv[3], NULL, 0);
+
+  // Initialize
+  int i;
+  srand(time(NULL));
+  pthread_mutex_init(&mutex, NULL);
+  sem_init(&empty, 0, BUFFER_SIZE); // All of buffer is empty
+  sem_init(&full, 0, 0);
+  count = in = out = 0;
+
+  // Create the producer and consumer threads
+  pthread_t producers[num_producer];
+  pthread_t consumers[num_consumer];
+  for(i = 0; i < num_producer; i++)
+    pthread_create(&producers[i], NULL, producer, NULL);
+  for(i = 0; i < num_consumer; i++)
+    pthread_create(&consumers[i], NULL, consumer, NULL);
+
+  // Sleep before terminating
+  sleep(stime);
+  return 0;
 }
-void signal(int *a)
-{
-	a++;
-}
-void *producer(void *pno)
-{   
-    int item;
-    while(true) {
-        item = rand(); // Produce an random item
-        wait(&empty);
-        wait(&mutex);
-        buffer[in] = item;
-        printf("Producer %d: Insert Item %d at %d\n", *((int *)pno),buffer[in],in);
-        in = (in+1)%BufferSize;
-        signal(&mutex);
-        signal(&full);
-    }
-}
-void *consumer(void *cno)
-{   
-    while(true) {
-        wait(&full);
-        wait(&mutex);
-        int item = buffer[out];
-        printf("Consumer %d: Remove Item %d from %d\n",*((int *)cno),item, out);
-        out = (out+1)%BufferSize;
-        signal(&mutex);
-        signal(&empty);
-    }
+
+// Insert item into buffer.
+//Returns 0 if successful, -1 indicating error
+int insert_item(buffer_item item){
+  int success;
+  sem_wait(&empty);
+  pthread_mutex_lock(&mutex);
+
+  // Add item to buffer
+  if( count != BUFFER_SIZE){
+    buffer[in] = item;
+    in = (in + 1) % BUFFER_SIZE;
+    count++;
+    success = 0;
+  }
+  else
+    success = -1;
+
+  pthread_mutex_unlock(&mutex);
+  sem_post(&full);
+  
+  return success;
 }
 
-int main()
-{   
+// Remove an object from the buffer, placing it in item.
+// Returns 0 if successful, -1 indicating error
+int remove_item(buffer_item *item){
+  int success;
+  
+  sem_wait(&full);
+  pthread_mutex_lock(&mutex);
+  
+  // Remove item from buffer to item
+  if( count != 0){
+    *item = buffer[out];
+    out = (out + 1) % BUFFER_SIZE;
+    count--;
+    success = 0;
+  }
+  else
+    success = -1;
 
-    pthread_t pro[5],con[5];
+  pthread_mutex_unlock(&mutex);
+  sem_post(&empty);
+  
+  return success;
+}
 
-    int a[5] = {1,2,3,4,5}; //Just used for numbering the producer and consumer
-
-    for(int i = 0; i < 5; i++) {
-        pthread_create(&pro[i], NULL, (void *)producer, (void *)&a[i]);
-    }
-    for(int i = 0; i < 5; i++) {
-        pthread_create(&con[i], NULL, (void *)consumer, (void *)&a[i]);
-    }
-
-    for(int i = 0; i < 5; i++) {
-        pthread_join(pro[i], NULL);
-    }
-    for(int i = 0; i < 5; i++) {
-        pthread_join(con[i], NULL);
-    }
-
-
-
-    return 0;
+void *producer(void *param){
+  buffer_item item;
+  while(1){
+    sleep(rand() % 5 + 1); // Sleep randomly between 1 and 5 seconds
     
+    item = rand();
+    if(insert_item(item))
+      printf("Error occured\n");
+    else
+      printf("Producer produced %d\n", item);
+  }
+}
+
+
+void *consumer(void *param){
+  buffer_item item;
+  while(1){
+    sleep(rand() % 5 + 1); // Sleep randomly between 1 and 5 seconds
+
+    if(remove_item(&item))
+      printf("Error occured\n");
+    else
+      printf("Consumer consumed %d\n", item);
+  }
 }
--- Tuần 5/Reader_Writer.c
+++ Tuần 5/Reader_Writer.c
@@ -1,72 +1,54 @@
-#include <pthread.h>
-#include <semaphore.h>
-#include <stdio.h>
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
 
-int wrt=1;
-int mutex=1;
+sem_t mutex,writeblock;
+int data = 0,rcount = 0;
 
-void wait(int *a)
+void *reader(void *arg)
 {
-	while(a<=0);
-	a--;
+  int f;
+  f = ((int)arg);
+  sem_wait(&mutex);
+  rcount = rcount + 1;
+  if(rcount==1)
+   sem_wait(&writeblock);
+  sem_post(&mutex);
+  printf("Data read by the reader%d is %d\n",f,data);
+  sleep(1);
+  sem_wait(&mutex);
+  rcount = rcount - 1;
+  if(rcount==0)
+   sem_post(&writeblock);
+  sem_post(&mutex);
 }
-void signal(int *a)
+
+void *writer(void *arg)
 {
-	a++;
-}
-
-int cnt = 1;
-int numreader = 0;
-
-void *writer(void *wno)
-{   
-    wait(&wrt);
-    cnt = cnt*2;
-    printf("Writer %d modified cnt to %d\n",(*((int *)wno)),cnt);
-    signal(&wrt);
-
-}
-void *reader(void *rno)
-{   
-    // Reader acquire the lock before modifying numreader
-    wait(&mutex);
-    numreader++;
-    if(numreader == 1) {
-        wait(&wrt); // If this id the first reader, then it will block the writer
-    }
-    signal(&mutex);
-    // Reading Section
-    printf("Reader %d: read cnt as %d\n",*((int *)rno),cnt);
-
-    // Reader acquire the lock before modifying numreader
-    wait(&mutex);
-    numreader--;
-    if(numreader == 0) {
-        signal(&wrt); // If this is the last reader, it will wake up the writer.
-    }
-    signal(&mutex);
+  int f;
+  f = ((int) arg);
+  sem_wait(&writeblock);
+  data++;
+  printf("Data writen by the writer%d is %d\n",f,data);
+  sleep(1);
+  sem_post(&writeblock);
 }
 
 int main()
-{   
-
-    pthread_t read[10],write[5];
-    int a[10] = {1,2,3,4,5,6,7,8,9,10}; //Just used for numbering the producer and consumer
-
-    for(int i = 0; i < 10; i++) {
-        pthread_create(&read[i], NULL, (void *)reader, (void *)&a[i]);
-    }
-    for(int i = 0; i < 5; i++) {
-        pthread_create(&write[i], NULL, (void *)writer, (void *)&a[i]);
-    }
-
-    for(int i = 0; i < 10; i++) {
-        pthread_join(read[i], NULL);
-    }
-    for(int i = 0; i < 5; i++) {
-        pthread_join(write[i], NULL);
-    }
-
-    return 0;
-    
+{
+  int i,b; 
+  pthread_t rtid[5],wtid[5];
+  sem_init(&mutex,0,1);
+  sem_init(&writeblock,0,1);
+  for(i=0;i<=2;i++)
+  {
+    pthread_create(&wtid[i],NULL,writer,(void *)i);
+    pthread_create(&rtid[i],NULL,reader,(void *)i);
+  }
+  for(i=0;i<=2;i++)
+  {
+    pthread_join(wtid[i],NULL);
+    pthread_join(rtid[i],NULL);
+  }
+  return 0;
 }
