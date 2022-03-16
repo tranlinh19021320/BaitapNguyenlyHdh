0821e431c23ac3257b779cad0ddf7cc0025a0a16
TranLinh21 <linhya21@gmail.com>
2022-03-16 17:13:38 +0700

    tuần 5

--- weak_5/Reader_Writer.c
+++ weak_5/Reader_Writer.c
@@ -7,14 +7,12 @@ int data = 0,rcount = 0;
 
 void *reader(void *arg)
 {
-  int f;
-  f = ((int)arg);
   sem_wait(&mutex);
   rcount = rcount + 1;
   if(rcount==1)
    sem_wait(&writeblock);
   sem_post(&mutex);
-  printf("Data read by the reader%d is %d\n",f,data);
+  printf("Data read by the reader%d is %d\n",(*((int *)arg)) ,data);
   sleep(1);
   sem_wait(&mutex);
   rcount = rcount - 1;
@@ -25,11 +23,9 @@ void *reader(void *arg)
 
 void *writer(void *arg)
 {
-  int f;
-  f = ((int) arg);
   sem_wait(&writeblock);
   data++;
-  printf("Data writen by the writer%d is %d\n",f,data);
+  printf("Data writen by the writer%d is %d\n",(*((int *)arg)) ,data);
   sleep(1);
   sem_post(&writeblock);
 }
@@ -40,12 +36,12 @@ int main()
   pthread_t rtid[5],wtid[5];
   sem_init(&mutex,0,1);
   sem_init(&writeblock,0,1);
-  for(i=0;i<=2;i++)
+  for(i=0;i<=5;i++)
   {
-    pthread_create(&wtid[i],NULL,writer,(void *)i);
-    pthread_create(&rtid[i],NULL,reader,(void *)i);
+    pthread_create(&wtid[i],NULL,writer,(void *)&(i+1));
+    pthread_create(&rtid[i],NULL,reader,(void *)&(i+1));
   }
-  for(i=0;i<=2;i++)
+  for(i=0;i<=5;i++)
   {
     pthread_join(wtid[i],NULL);
     pthread_join(rtid[i],NULL);
